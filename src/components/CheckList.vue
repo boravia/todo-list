824e114e448bbfc09cd5c789859f6e0c70b799fa
Adilkhan <135254828+boravia@users.noreply.github.com>
2024-05-27 22:08:28 +0500

    code cleaning

--- src/components/CheckList.vue
+++ src/components/CheckList.vue
@@ -1,30 +1,15 @@
 <template>
   <ul class="list">
-    <li class="list_element">
-      <div class="circle"></div>
-      <span class="list_text">Download App</span>
-      <button class="remove_btn">
-        <Icon icon="ph:trash" />
-      </button>
-    </li>
-    <li class="list_element">
-      <div class="circle"></div>
-      <span class="list_text">Use App</span>
-      <button class="remove_btn">
-        <Icon icon="ph:trash" />
-      </button>
-    </li>
-    <ListItem :item="{id: item.id, text: item.text, completed: item.completed}" v-for="item in items" :key="item.id"></ListItem>
+    <ListItem :item="item" v-for="item in items" :key="item.id"></ListItem>
   </ul>
 </template>
 
 <script>
-import { Icon } from '@iconify/vue';
 import ListItem from './ListItem.vue';
 
 export default {
   components: {
-    Icon, ListItem,
+    ListItem,
   },
   data() {
     return{
